d5322823a47a50f9f1b4bd0dfc23b40e89cdcc87
orenjiakira <orenjiakira@gmail.com>
2016-12-06 15:17:13 -0200

    Make interactive char select menu

--- src/hud/menu/char_select.gd
+++ src/hud/menu/char_select.gd
@@ -2,29 +2,64 @@
 extends "res://hud/menu/screen.gd"
 
 const CHAR_ITEM = preload("res://hud/menu/char_select/char_item.tscn")
+const COLS = 3
 
 onready var chars = get_node("/root/database/chars").get_children()
-onready var cursor1 = get_node("cursor1")
-onready var cursor2 = get_node("cursor2")
+onready var cursors = [ get_node("cursor1"), get_node("cursor2") ]
 onready var lists = [ get_node("list1"), get_node("list2") ]
-
-var p1_selection = 0
-var p2_selection = 0
+var selection = [ 0, 0 ]
 
 func _ready():
-  var j = 0
-  var y = 5 * 64
-  for chara in chars:
-    for i in range(2):
-      var pos = Vector2(i * 512 + 128 + (j % 3) * 128, y )
-      var item = CHAR_ITEM.instance()
-      item.set_pos(pos)
-      lists[i].add_child(item)
-      item.set_char(chara.get_name())
-    j += 1
-    if j % 3 == 0:
-      y += 128
-
+  for n in range(chars.size()):
+    for pl in range(2):
+      add_char_item(pl, n)
+  for pl in range(2):
+    update_cursor_pos(pl)
 
 func _on_press_action(action):
-  pass
+  var pl = floor(action / ACTIONS.P2_UP)
+  var act = action % ACTIONS.P2_UP
+  if act < 4:
+    move_selection(pl, act)
+  else:
+    handle_action(pl, act)
+
+func add_char_item(pl, n):
+  var name = chars[n].get_name()
+  var char = CHAR_ITEM.instance()
+  var x = pl * 512 + 128 + (n % COLS) * 128
+  var y = 5 * 64 + 128 * floor(n / COLS)
+  lists[n].add_child(char)
+  char.set_pos(Vector2(x, y))
+  char.set_char(name)
+
+func move_selection(pl, dir):
+  var n = chars.size()
+  if dir == 0:
+    selection[pl] += 2 * n - COLS
+  elif dir == 1:
+    selection[pl] += 1
+  elif dir == 2:
+    selection[pl] += COLS
+  elif dir == 3:
+    selection[pl] += n - 1
+  selection[pl] = selection[pl] % n
+  print("SELECTION P", pl, ": ", selection[pl])
+  update_cursor_pos(pl)
+
+func handle_action(pl, act):
+  if act == 4:
+    pass
+  elif act == 6:
+    pass
+  elif act == 7:
+    pass
+  elif act == 8:
+    pass
+
+func update_cursor_pos(pl):
+  var cursor = cursors[pl]
+  var n = selection[pl]
+  var x = pl * 512 + 128 + (n % COLS) * 128
+  var y = 5 * 64 + 128 * floor(n / COLS)
+  cursor.set_pos(Vector2(x, y))
