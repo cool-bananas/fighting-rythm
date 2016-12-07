da08ae90f70680ea1991610bb54b3e6ee2eceae5
Enzo Hideki <enzo25estrela@gmail.com>
2016-12-07 03:40:55 -0200

    Andar certo

--- src/player/animation.gd
+++ src/player/animation.gd
@@ -3,7 +3,7 @@ extends Sprite
 
 onready var animation = get_node("animation")
 
-func _on_change_state( st ):
+func _on_change_state(st):
 	if animation.has_animation(st):
 		animation.stop(true)
 		animation.play(st)
